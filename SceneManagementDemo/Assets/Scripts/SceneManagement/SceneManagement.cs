068c0664f77ee6dd57d59800d50e11270e23eaca
Daniel <johnson512n28@Hotmail.com>
2017-01-26 21:22:09 -0500

    Scene Management Demo
    Added basic scenes and scripts

---
+++ SceneManagementDemo/Assets/Scripts/SceneManagement/SceneManagement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+namespace VRSceneManagement
+{
+	public interface SceneState
+	{
+		void Load ( );
+
+		void Unload ();
+	}
+
+	public static class VRSceneManager {
+		private static SceneState m_CurrentState = null;
+
+		public static void LoadSceneState( SceneState newState )
+		{
+			if (m_CurrentState != null && newState != m_CurrentState)
+				m_CurrentState.Unload ();
+
+			m_CurrentState = newState;
+			m_CurrentState.Load ();
+		}
+
+		public static void LoadScene( string newScene )
+		{
+			SceneManager.LoadScene (newScene);
+		}
+	}
+}
