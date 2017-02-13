9ea2d8814c1b256015767c33d151271153c1f833
Daniel <johnson512n28@Hotmail.com>
2017-02-12 23:20:24 -0500

    Map Generation Demo

---
+++ SceneManagementDemo/Assets/Scripts/SceneManagement/MapSceneManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRSceneManagement;
+
+public class MapSceneManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		VRSceneManager.LoadSceneState( this.GetComponent<MapOverviewState>() );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
